a409cf97b53df6343ff844415884e370f4e5bf55
Ravi Bechoe <35457924+rbechoe@users.noreply.github.com>
2022-04-08 20:20:34 +0200

    Update DelayedMethods.cs

--- Dialogue Builder/Assets/Scripts/Systems/DelayedMethods.cs
+++ Dialogue Builder/Assets/Scripts/Systems/DelayedMethods.cs
@@ -19,9 +19,9 @@ public static class DelayedMethods<T>
     }
 }
 
-public static class DelayedMethods<T, V>
+public static class DelayedMethods<T1, T2>
 {
-    public static IEnumerator DelayedMethod(System.Action<T, V> method, T arg1, V arg2, float wait = 0f)
+    public static IEnumerator DelayedMethod(System.Action<T1, T2> method, T1 arg1, T2 arg2, float wait = 0f)
     {
         yield return new WaitForSeconds(wait);
         method.Invoke(arg1, arg2);
